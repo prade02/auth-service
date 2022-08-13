531cec28e9435b16a0f352ab012b24d36005bd3f
Pradeep Kumar Manimuthu <prade.ycsm@gmail.com>
2022-08-13 23:46:32 +0530

    New store proc to retrieve role hierarchy expression

--- database/auth_service.sql
+++ database/auth_service.sql
@@ -20,3 +20,22 @@ CREATE TABLE public.user_role
         ON DELETE NO ACTION
         NOT VALID
 );
+
+-- PROCEDURE: public.sp_get_role_hierarchy_expression()
+
+-- DROP PROCEDURE IF EXISTS public.sp_get_role_hierarchy_expression();
+
+CREATE OR REPLACE PROCEDURE public.sp_get_role_hierarchy_expression(
+	OUT role_hierarchy_expression character varying)
+LANGUAGE 'sql'
+AS $BODY$
+WITH RECURSIVE reachable_roles AS (
+	SELECT id, parent_authority_name, authority FROM user_authority WHERE parent_authority IS NULL
+	UNION ALL
+	SELECT uAuthority.id, uAuthority.parent_authority_name, uAuthority.authority FROM user_authority uAuthority INNER JOIN reachable_roles reachable ON
+	uAuthority.parent_authority = reachable.id
+)
+SELECT string_agg(parent_authority_name || ' > ' || authority, ' \n ') as authority_hierarchy FROM reachable_roles WHERE parent_authority_name IS NOT NULL;
+$BODY$;
+ALTER PROCEDURE public.sp_get_role_hierarchy_expression()
+    OWNER TO postgres;
